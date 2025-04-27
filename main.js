0c48981dfb8e6266f630a0dd6fb5193291cd2b18
Nicolas Ardila <jackas779@gmail.com>
2025-04-27 00:35:04 -0500

    se actualiza forma de crear tareas para crear un archivo json donde se almacenen las tareas

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 #!/usr/bin/env node
 
+import { createDirname } from './utils/path.js'
 import fs from 'fs'
+import path from 'path'
+
+const { __dirname } = createDirname(import.meta.url)
+const TASK_FILE = 'tasks.json'
+
+const pathArchive = path.join(__dirname, TASK_FILE)
+
+if (!fs.existsSync(pathArchive)) {
+
+  try {
+    fs.writeFileSync(pathArchive, JSON.stringify([]), 'utf-8')
+  } catch (error) {
+    console.log(`error al crear el archivo ${TASK_FILE}`);
+  }
+
+}
 
 // 1. Obtener el objeto Date para el momento actual
 const fechaActual = new Date();
@@ -31,9 +48,16 @@ const segundosFormateados = String(segundos).padStart(2, '0');
 const date = `${dia} de ${nombreMes} de ${anio} a las ${horasFormateadas}:${minutosFormateados}:${segundosFormateados}`;
 
 
-const TASK_FILE = 'tasks.json'
+let tasks = []
 
-const tasks = []
+try {
+  const data = fs.readFileSync(pathArchive, 'utf-8')
+  const parseData = JSON.parse(data)
+  tasks = parseData
+} catch (error) {
+  console.log('error al leer el archivo', error);
+  exit()
+}
 
 const commands = {
   add: createTask,
@@ -51,25 +75,38 @@ function createTask(task) {
 
 
   const nameTask = task.trim().toLowerCase()
+  const taskCreated = tasks.find(e => e.name === nameTask)
 
   if (nameTask.trim() === '') {
     console.log("La tarea no puede estar vacia");
     return
   }
-  tasks.push({
-    name: nameTask,
-    // date: `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`,
-    date: date,
-    status: 'no iniciado',
-  })
-
-  const taskCreated = tasks.find(e => e.name === nameTask)
-  if (!taskCreated) {
-    console.log("no se pudo crear correctamente la tarea");
+  if (taskCreated) {
+    console.log("la tarea ya existe");
+    return
   }
+
+  try {
+    tasks.push({
+      id: tasks.length + 1,
+      name: nameTask,
+      dateCreate: date,
+      dateUpdate: '',
+      status: 0,
+    })
+  } catch (error) {
+    console.log("no se pudo crear correctamente la tarea", error);
+    return
+  }
+
   console.log("la tarea fue creada correctemente");
 
-  console.log(tasks);
+
+  try {
+    fs.writeFileSync(pathArchive, JSON.stringify(tasks), 'utf-8')
+  } catch (error) {
+    console.log(`error al crear el archivo ${TASK_FILE}`);
+  }
 
 
 }
