1b50445487d5a1239e17a28d98e315f21e933b66
Nicolas Ardila <jackas779@gmail.com>
2025-04-28 23:39:47 -0500

    Se cambia la logica para coincidir con la nueva estructura para incluir un controlador y un modelo

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,21 +1,84 @@
-export default class TaskModel {
-  static add () {
-
+export default class TaskController {
+  constructor (command, data, taskModel) {
+    const action = data.argument.toLowerCase().trim()
+    const action2 = data.argument2.toLowerCase().trim()
+    this.command = command.trim().toLowerCase()
+    this.data = { argument: action, argument2: action2 }
+    this.objetTask = {}
+    this.commands = ['add', 'update', 'list', 'delete', 'mark-done', 'mark-in-progress', 'help']
+    this.taskModel = taskModel
   }
 
-  static update () {
+  run (route) {
+    if (!this.commands.includes(this.command)) {
+      console.log('Comando no reconocido para mas informacion escriba help')
+    }
 
-  }
+    const { argument, argument2 } = this.data
+    const arrayTask = this.taskModel.getData(route)
+    switch (this.command) {
+      case 'add' :{
+        this.objetTask = { task: argument }
+        const task = this.taskModel.add(this.objetTask, arrayTask, route)
+        console.log(task)
+        break
+      }
+      case 'update':{
+        this.objetTask = { id: argument, newTask: argument2 }
+        const task = this.taskModel.update(this.objetTask, arrayTask, route)
+        console.log(task)
+        break
+      }
 
-  static delete () {
+      case 'list': {
+        this.objetTask = { method: argument }
+        const taskFilter = this.taskModel.list(this.objetTask, arrayTask)
+        if (taskFilter === 'no valid') {
+          console.log('Comando no valido para mas informacion por favor escriba el comando help')
+          break
+        }
+        if (taskFilter.length === 0 || taskFilter.length === undefined) {
+          console.log('No hay tareas para mostrar')
+          break
+        }
+        taskFilter.forEach(task => {
+          let status = ''
+          if (task.status === 1) {
+            status = 'No iniciada'
+          }
+          if (task.status === 2) {
+            status = 'En progreso'
+          }
+          if (task.status === 3) {
+            status = 'Finalizada'
+          }
+          console.log(`ID : ${task.id} -> Nombre: ${task.name} -> Estado: ${status}(${task.status}) ||| Fecha creacion: ${task.dateCreate} --- Fecha Ultima Actualizacion: ${task.dateUpdate}`)
+        })
+        break
+      }
+      case 'delete' :{
+        this.objetTask = { id: argument }
+        const task = this.taskModel.delete(this.objetTask, arrayTask, route)
+        console.log(task)
+        break
+      }
 
-  }
+      case 'mark-done':{
+        this.objetTask = { id: argument, option: 2 }
+        const updateTask = this.taskModel.updateStatus(this.objetTask, arrayTask, route)
+        console.log(updateTask)
+        break
+      }
 
-  static list () {
-
-  }
-
-  static updateStatus () {
-
+      case 'mark-in-progress':{
+        this.objetTask = { id: argument, option: 1 }
+        const updateTask = this.taskModel.updateStatus(this.objetTask, arrayTask, route)
+        console.log(updateTask)
+        break
+      }
+      default:
+        console.log('comando no valido')
+        break
+    }
   }
 }
--- models/task.model.js
+++ models/task.model.js
@@ -1,47 +1,202 @@
-import TaskModel from '../controllers/task.model'
+import { date } from '../utils/date.js'
+import fs from 'fs'
 
-export default class TaskController {
-  constructor (command, data) {
-    this.command = command.trim().toLowerCase()
-    this.data = data
-    this.objetTask = {}
-    this.commands = ['add', 'update', 'list', 'delete', 'mark-done', 'mark-in-progress', 'help']
+export default class TaskModel {
+  static getData (route) {
+    let tasks = []
+
+    try {
+      const data = fs.readFileSync(route, 'utf-8')
+      const parseData = JSON.parse(data)
+      tasks = parseData
+    } catch (error) {
+      console.log('error al leer el archivo', error)
+      process.exit()
+    }
+
+    return tasks
   }
 
-  run () {
-    // console.log(this.command)
-    // console.log(this.data)
-
-    if (!this.commands.includes(this.command)) {
-      console.log('Comando no reconocido para mas informacion escriba help')
+  static updateData (route, tasks) {
+    try {
+      fs.writeFileSync(route, JSON.stringify(tasks), 'utf-8')
+    } catch (error) {
+      console.log('error', error)
+      return false
     }
 
-    const { argument, argument2 } = this.data
+    return true
+  }
 
-    switch (this.command) {
-      case 'add' :
-        this.objetTask = { task: argument }
-        break
-      case 'update':
-        this.objetTask = { id: argument, newTask: argument2 }
-        break
-      case 'list':
-        this.objetTask = { option: argument }
-        break
-      default:
-        this.objetTask = { id: argument }
-        break
-      // case 'delete' :
-      //   this.objetTask = { id: this.argument }
-      //   break
-      // case 'mark-done':
-      //   this.objetTask = { id: this.argument }
-      //   break
-      // case 'mark-in-progress':
-      //   this.objetTask = { id: this.argument }
-      //   break
+  static add (argument, tasks, route) {
+    const { task } = argument
+    const nameTask = task.trim().toLowerCase()
+    const taskCreated = tasks.find(e => e.name === nameTask)
+    const idTask = tasks.length + 1
+
+    if (nameTask.trim() === '') {
+      return 'La tarea no puede estar vacia'
+    }
+    if (taskCreated) {
+      return 'la tarea ya existe'
     }
 
-    console.log(this.objetTask)
+    try {
+      tasks.push({
+        id: idTask,
+        name: nameTask,
+        dateCreate: date,
+        dateUpdate: '',
+        status: 1
+      })
+    } catch (error) {
+      return ('no se pudo crear correctamente la tarea', error)
+    }
+
+    const result = this.updateData(route, tasks)
+
+    if (!result) {
+      return 'No se pudo crear la nueva tarea'
+    }
+
+    return `la tarea fue creada correctemente (ID: ${idTask}) `
+  }
+
+  static update (argument, tasks, route) {
+    if (tasks.length === 0 || tasks.length === undefined) {
+      return 'No hay tareas para actualizar'
+    }
+
+    const { id, newTask } = argument
+    let idTask
+
+    if (idTask === '') {
+      return 'El identificador no puede estar vacio'
+    }
+
+    try {
+      idTask = parseInt(id)
+    } catch (e) {
+      return 'El identificador de la tarea debe ser un numero'
+    }
+
+    if (newTask === '') {
+      return 'La nueva tarea no puede estar vacia'
+    }
+    const taskRepeit = tasks.find(task => task.name === newTask)
+
+    if (taskRepeit) {
+      return 'Esta tarea ya esta asignada'
+    }
+
+    const taskFind = tasks.find(task => task.id === idTask)
+
+    if (!taskFind) {
+      return 'La tarea no fue encontrada'
+    }
+
+    taskFind.name = newTask
+    taskFind.dateUpdate = date
+
+    const result = this.updateData(route, tasks)
+
+    if (!result) {
+      return 'No se pudo actualizar la tarea'
+    }
+
+    return `la tarea fue actualizada correctemente (ID: ${idTask}) -> name : ${newTask} `
+  }
+
+  static delete (argument, tasks, route) {
+    if (tasks.length === 0 || tasks.length === undefined) {
+      return 'No hay tareas para eliminar'
+    }
+    const { id } = argument
+    if (id === '' || id === undefined) {
+      return 'El identificador de la tarea no puede estar vacio'
+    }
+
+    let idTask
+    try {
+      idTask = parseInt(id)
+    } catch (error) {
+      return 'El identificador tiene que ser un numero'
+    }
+
+    const taskFind = tasks.find(e => e.id === idTask)
+    if (!taskFind) {
+      return 'Identificador no encontrado'
+    }
+
+    const newTasks = tasks.filter(task => task.id !== taskFind.id)
+
+    const result = this.updateData(route, newTasks)
+    if (!result) {
+      return 'no se pudo eliminar la tarea'
+    }
+
+    return 'Tarea eliminada correctamente'
+  }
+
+  static list (option, tasks) {
+    const { method } = option
+    if (method === 'done') {
+      return tasks.filter(task => task.status === 3)
+    }
+    if (method === 'todo') {
+      return tasks.filter(task => task.status === 1)
+    }
+    if (method === 'in-progress') {
+      return tasks.filter(task => task.status === 2)
+    }
+    if (method === '') {
+      return tasks
+    }
+
+    return 'no valid'
+  }
+
+  static updateStatus (argument, tasks, route) {
+    if (tasks.length === 0 || tasks.length === undefined) {
+      return 'No hay tareas por favor cree una tarea'
+    }
+    const { id, option } = argument
+    let idTask
+    let mensajeFinal = ''
+    let newStatus
+    if (option === 1) {
+      mensajeFinal = 'La tarea se marco como en progreso'
+      newStatus = 2
+    }
+    if (option === 2) {
+      mensajeFinal = 'La tarea se marco como terminada'
+      newStatus = 3
+    }
+
+    if (id === '' || id === undefined) {
+      console.log('El identificador de la tarea no puede estar vacio')
+      return
+    }
+
+    try {
+      idTask = parseInt(id)
+    } catch (error) {
+      return 'El identificador tiene que ser un numero'
+    }
+
+    const taskFind = tasks.find(task => task.id === idTask)
+    if (!taskFind) {
+      return 'No se encontro la tarea'
+    }
+
+    taskFind.status = newStatus
+    taskFind.dateUpdate = date
+
+    const result = this.updateData(route, tasks)
+    if (!result) {
+      return `error al actualizar el estado de la tarea ( ID: ${idTask} )`
+    }
+
+    return mensajeFinal
   }
 }
